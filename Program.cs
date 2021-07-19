77ec2c1d0b3a0461aab109cfa12ed618db8445d3
Andrii Kurdiumov <kant2002@gmail.com>
2021-07-19 19:58:04 +0600

    Add static website

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
-using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
-namespace HelloWorld
+namespace tinykestrel
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter you name:");
-            var greeting = Console.ReadLine();
-            Console.WriteLine($"Hello {greeting}!");
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .Configure(app =>
+                {
+                    app.Run(context => context.Response.WriteAsync("Hello World"));
+                })
+                .Build();
+            host.Run();
         }
     }
 }
