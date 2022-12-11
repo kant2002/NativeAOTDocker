b60558fdddedfeb54abce8619669a78baabd39ff
Andrii Kurdiumov <kant2002@gmail.com>
2022-12-11 21:09:23 +0600

    Add HTTP request test

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ Console.WriteLine("Enter you name:");
 var greeting = Console.ReadLine();
 Console.WriteLine($"Hello {greeting}!");
 double num = 5.2;
+
+Console.WriteLine("Querying first website:");
+using var client = new HttpClient();
+var content = await client.GetStringAsync("http://info.cern.ch/");
+Console.WriteLine(content);
 #if NO_GLOBALIZATION
 Console.WriteLine($"Number 5.2 with invariant culture {num.ToString("F2")}");
 #else
