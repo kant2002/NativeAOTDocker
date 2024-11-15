589d7561141c9e5193d3ed2ca65bc34eace621dd
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-15 16:14:36 +0500

    Add chiseled configuration

--- build.ps1
+++ build.ps1
@@ -4,6 +4,7 @@ docker build -t nativeaot-scratch-compression CompressionEmbedding
 docker build -t nativeaot-scratch-openssl OpenSslEmbedding
 docker build -t nativeaot-scratch-http-client -f OpenSslEmbedding/Dockerfile.nossl OpenSslEmbedding
 docker build -t nativeaot-scratch-webapi WebApi
+docker build -t nativeaot-scratch-webapi-chiseled WebApiChiseled
 docker build -t nativeaot-scratch-grpcapi GrpcApi
 docker build -t nativeaot-scratch-npgsql NpgCli
 docker build -t nativeaot-scratch-npgsql-noicu NpgCli -f NpgCli/Dockerfile.noicu
