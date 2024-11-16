33b4575134bfb0a3f1cdffe96abc18ab4a18b308
Andrii Kurdiumov <kant2002@gmail.com>
2024-11-16 12:36:56 +0500

    Add NpgDataBuilderSlim experiment

---
+++ NpgCliSlim/Program.cs
@@ -0,0 +1,12 @@
+﻿using Npgsql;
+
+var connectionString = Environment.GetEnvironmentVariable("ConnectionString") ?? "Host=localhost:32768;Username=postgres;Password=postgrespw";
+Console.WriteLine("Connecting using connection string "+ connectionString);
+using var conn = new NpgsqlSlimDataSourceBuilder(connectionString).Build();
+
+await using (var cmd = conn.CreateCommand("SELECT 5"))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+while (await reader.ReadAsync())
+    Console.WriteLine(reader.GetInt32(0));
+}
--- build.ps1
+++ build.ps1
@@ -1,10 +1,14 @@
-docker build -t nativeaot-scratch EmbeddedICU 
-docker build -t nativeaot-scratch-invariant InvariantGlobalization
-docker build -t nativeaot-scratch-compression CompressionEmbedding
-docker build -t nativeaot-scratch-openssl OpenSslEmbedding
-docker build -t nativeaot-scratch-http-client -f OpenSslEmbedding/Dockerfile.nossl OpenSslEmbedding
-docker build -t nativeaot-scratch-webapi WebApi
-docker build -t nativeaot-scratch-webapi-chiseled WebApiChiseled
-docker build -t nativeaot-scratch-grpcapi GrpcApi
-docker build -t nativeaot-scratch-npgsql NpgCli
-docker build -t nativeaot-scratch-npgsql-noicu NpgCli -f NpgCli/Dockerfile.noicu
+$containerTool="docker"
+&$containerTool build -t nativeaot-scratch EmbeddedICU 
+&$containerTool build -t nativeaot-scratch-invariant InvariantGlobalization
+&$containerTool build -t nativeaot-scratch-compression CompressionEmbedding
+&$containerTool build -t nativeaot-scratch-openssl OpenSslEmbedding
+&$containerTool build -t nativeaot-scratch-http-client -f OpenSslEmbedding/Dockerfile.nossl OpenSslEmbedding
+&$containerTool build -t nativeaot-scratch-webapi WebApi
+&$containerTool build -t nativeaot-scratch-grpcapi GrpcApi
+&$containerTool build -t nativeaot-scratch-npgsql NpgCli
+&$containerTool build -t nativeaot-scratch-npgsql-noicu NpgCli -f NpgCli/Dockerfile.noicu
+&$containerTool build -t nativeaot-scratch-npgsql-aot NpgCliSlim
+&$containerTool build -t nativeaot-scratch-npgsql-aot-noicu NpgCliSlim -f NpgCliSlim/Dockerfile.noicu
+
+&$containerTool build -t nativeaot-scratch-webapi-chiseled WebApiChiseled
